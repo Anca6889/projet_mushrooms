046f137691a57fc2a2200665965c4be00acd5d7b
Jean-Maxime G <71003854+Anca6889@users.noreply.github.com>
2022-01-19 22:19:22 +0100

    passed DEBUG ON TRUE for spot error 500

--- projectmushrooms/settings.py
+++ projectmushrooms/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False if os.environ.get("ENV", "development") == "production" else True
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapps.com']
 
