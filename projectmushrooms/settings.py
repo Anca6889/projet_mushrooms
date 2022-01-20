76af7274acef17082e475b5df1db6ea1207bce2a
Jean-Maxime G <71003854+Anca6889@users.noreply.github.com>
2022-01-21 00:49:56 +0100

    modif variable environnement DEBUG

--- projectmushrooms/settings.py
+++ projectmushrooms/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False if os.environ.get("ENV", "development") == "production" else True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['.herokuapps.com', 'localhost', '127.0.0.1']
 
