44079c300cad687a2ed96451380ff521ebe0b295
Jean-Maxime G <71003854+Anca6889@users.noreply.github.com>
2022-01-20 09:38:40 +0100

    modif wsgi

--- projectmushrooms/wsgi.py
+++ projectmushrooms/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projectmushrooms.settings')
+
+application = WhiteNoise(application, root='/static/assets/img/')
+application.add_files('/static/assets/img/', prefix='more-files/')
 
 application = get_wsgi_application()
