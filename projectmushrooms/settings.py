3ac0ad57dc5cc3c6a4eb4b1da53aaf26b85f484c
Jean-Maxime G <71003854+Anca6889@users.noreply.github.com>
2022-01-20 10:28:14 +0100

    install Sentry 2

--- projectmushrooms/settings.py
+++ projectmushrooms/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
