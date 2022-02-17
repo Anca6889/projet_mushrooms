25bcef0857f34c163c8ae1c5bbdf56fa688f6d43
Jean-Maxime G <71003854+Anca6889@users.noreply.github.com>
2022-02-17 09:29:27 +0100

    Add travis + modif police responsive pour fiche champignon

---
+++ projectmushrooms/travis.py
@@ -0,0 +1,14 @@
+import settings
+
+SECRET_KEY = "Travis_test"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
